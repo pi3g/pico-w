7aa0e288073004c062194396a4fd82e69b508873
NathanBusler <79189846+NathanBusler@users.noreply.github.com>
2022-07-11 23:05:03 +0200

    Add files via upload

---
+++ MicroPython/III Wi-Fi Doorbell/pico_w_doorbell.py
@@ -0,0 +1,86 @@
+import rp2
+import network
+import ubinascii
+import machine
+import urequests as requests
+import time
+from secrets import secrets
+import socket
+
+# Set country to avoid possible errors
+rp2.country('DE')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+# If you need to disable powersaving mode
+# wlan.config(pm = 0xa11140)
+
+# See the MAC address in the wireless chip OTP
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+print('mac = ' + mac)
+
+# Other things to query
+# print(wlan.config('channel'))
+# print(wlan.config('essid'))
+# print(wlan.config('txpower'))
+
+# Load login data from different file for safety reasons
+ssid = secrets['ssid']
+pw = secrets['pw']
+
+wlan.connect(ssid, pw)
+
+# Wait for connection with 10 second timeout
+timeout = 10
+while timeout > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    timeout -= 1
+    print('Waiting for connection...')
+    time.sleep(1)
+
+# Define blinking function for onboard LED to indicate error codes    
+def blink_onboard_led(num_blinks):
+    led = machine.Pin('LED', machine.Pin.OUT)
+    for i in range(num_blinks):
+        led.on()
+        time.sleep(.2)
+        led.off()
+        time.sleep(.2)
+    
+# Handle connection error
+# Error meanings
+# 0  Link Down
+# 1  Link Join
+# 2  Link NoIp
+# 3  Link Up
+# -1 Link Fail
+# -2 Link NoNet
+# -3 Link BadAuth
+
+wlan_status = wlan.status()
+blink_onboard_led(wlan_status)
+
+if wlan_status != 3:
+    raise RuntimeError('Wi-Fi connection failed')
+else:
+    print('Connected')
+    status = wlan.ifconfig()
+    print('ip = ' + status[0])
+    
+door_bell = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+last_state = False
+current_state = False
+
+while True:
+    current_state = door_bell.value()
+    if last_state == False and current_state == True:
+        # Make a GET request to trigger webhook on IFTTT
+        # ifttt_url = 'https://maker.ifttt.com/trigger/pico_w_request/with/key/cGoabCNRjfBnQyWxbg-xe7tA6Q_Uma8uMmX3Qa2vUNP'
+        ifttt_url = 'https://maker.ifttt.com/trigger/door_bell_rang/with/key/cGoabCNRjfBnQyWxbg-xe7tA6Q_Uma8uMmX3Qa2vUNP'
+        request = requests.get(ifttt_url)
+        print(request.content)
+        request.close()
+        time.sleep(2)
+    last_state = current_state
---
+++ MicroPython/III Wi-Fi Doorbell/secrets.py
@@ -0,0 +1,9 @@
+secret = {
+    'ssid': 'couchtiere',
+    'pw': '95624610657896356407'
+    }
+
+secrets = {
+    'ssid': 'Vodafone-669025',
+    'pw': '5GxkqSdRnP94Mu2f'
+    }
